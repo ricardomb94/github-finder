2bc308ceca9303038e46152a52efb3cd19625fb6
ricardomb94 <aymarmb@gmail.com>
2021-04-25 17:44:07 +0200

    Project set up

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React, {Component} from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+import UserItem from './components/users/UserItem'
+
+class App extends Component{
+  
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
+  }
+  
+
+export default App;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ class UserItem extends Component {
+     constructor(){
+         super();
+         console.log('hey Ric');
+     }
+    render() {
+        return (
+            <div>
+                User-item
+            </div>
+        )
+    }
+}
+
+export default UserItem
