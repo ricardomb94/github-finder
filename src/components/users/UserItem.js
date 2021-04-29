765cdf1eda6e0767b42aff46177b8031f873869d
ricardomb94 <aymarmb@gmail.com>
2021-04-29 16:12:26 +0200

    Routine

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
- class UserItem extends Component {
+ const UserItem = (props) => {
     
-    render() {
-        const {login, avatar_url, html_url} = this.props.user;
+        const {login, avatar_url, html_url} = props.user;
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="portrait" className="round-img" style={{width:'60px'}}/>
@@ -15,7 +14,6 @@ import React, { Component } from 'react'
                 </div>
             </div>
         )
-    }
 }
 
 export default UserItem
